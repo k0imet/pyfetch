eefcb45a1280b04e795cafe24372261190d6b5bf
koimet <60982828+k0imet@users.noreply.github.com>
2022-12-05 15:20:16 +0300

    added a date functionality
    😄

--- send_cve_list.sh
+++ send_cve_list.sh
@@ -9,8 +9,14 @@ INTERVAL="week"
 # Set the flag for including the CVE info in the output
 INFO_FLAG="-info"
 
+# Get the current date in the format YYYY-MM-DD
+DATE=$(date +%F)
+
 # Run the pyfetch script and save the output in a variable
 OUTPUT=$(python pyfetch $INTERVAL $INFO_FLAG)
 
+# Insert the date into the email subject string
+SUBJECT="CVE List - $DATE"
+
 # Send the output to the specified email address
-echo "$OUTPUT" | mail -s "CVE List" $EMAIL_ADDRESS
+echo "$OUTPUT" | mail -s "$SUBJECT" $EMAIL_ADDRESS
